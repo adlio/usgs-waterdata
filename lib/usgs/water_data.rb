525fd1edc60ce36ed24f8c0720f57e1a45342d5a
Aaron Longwell <aaron@eeepc.(none)>
2008-10-10 23:19:10 -0700

    Finished basic test of us_rivers function. Able to retrieve data from all U.S. states' rivers.

--- lib/usgs/water_data.rb
+++ lib/usgs/water_data.rb
@@ -4,12 +4,61 @@ module USGS
 
     class << self
 
-      def update_state(st)
-        require 'open-uri'
-        url = "http://waterdata.usgs.gov/#{st}/nwis/current/?type=flow&format=rdb"
-        open(url).each do |line|
-          puts line
+      def us_rivers
+        states = %w{al ar az ca co ct dc de fl ga hi ia id il in ks ky la ma md me mi mn mo ms mt nc nd ne nh nj nm nv ny oh ok or pa ri sc sd tn tx ut va vt wa wi wv wy}
+        state_rivers = {}
+        states.each do |state|
+          STDOUT.write("#{state} ") # REMOVE THIS REMOVE THIS REMOVE THIS
+          STDOUT.flush
+          state_rivers[state] = rivers_in_state(state)
         end
+        state_rivers
+      end
+
+
+      def rivers_in_state(st)
+        require 'open-uri'
+
+        url = "http://waterdata.usgs.gov/#{st}/nwis/current/?type=flow&format=rdb"
+        rivers = {}
+
+        open(url).each do |line|
+          next if line =~ /^#/
+          next if line =~ /^5/
+          next if line =~ /^agency/
+
+          fields = line.split(/\t/)
+          station_name = fields[2]
+          name_parts = station_name.split(/\b([A-Z]+ OF|ABV|AT|NR|NEAR|DS OF|US OF|ABOVE|BELOW|BEL|BL|BLW|AB|ABV)\b/i)
+          river_name = clean_river_name(name_parts[0])
+
+          rivers[river_name] ||= []
+          rivers[river_name] << fields[1]
+        end
+
+        rivers
+      end
+
+
+      # Given a river name from the USGS site, a clean and 
+      # normalized river name is returned.
+      #
+      def clean_river_name(river_name)
+        val = river_name.strip.downcase.gsub(/\b('?[a-z])/) { $1.capitalize }
+        val.gsub!(/d'?\s*alene/i, "d'Alene")
+        val.gsub!(/[^A-Za-z' ]/, '')
+        val.gsub!(/\bSf\b/, 'South Fork')
+        val.gsub!(/\bMf\b/, 'Middle Fork')
+        val.gsub!(/\bNf\b/, 'North Fork')
+        val.gsub!(/\bDiv\b/, 'Diversion')
+        val.gsub!(/\bCrk?\b/, 'Creek')
+        val.gsub!(/\bC\b/, 'Creek')
+        val.gsub!(/\bR\b/, 'River')
+        val.gsub!(/\bBra?(nch)?\b/, 'Branch')
+        val.gsub!(/\bRvr\b/, 'River')
+        val.gsub!(/\bTrib\b/, 'Tributary')
+        val.gsub!(/\bSprngs\b/, 'Springs')
+        val
       end
 
     end
--- spec/waterdata_spec.rb
+++ spec/waterdata_spec.rb
@@ -1,4 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 
 describe USGS::WaterData do
+
+  it 'should retrieve data' do
+    USGS::WaterData.us_rivers.each do |state, rivers|
+      puts "#{state}"
+      puts '-' * 60
+      rivers.each do |river_name, gauge_ids|
+        puts " * #{river_name} => #{gauge_ids.join(', ')}"
+      end
+    end
+  end
+
 end
