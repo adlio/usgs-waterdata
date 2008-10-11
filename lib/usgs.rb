af8c77254539a094babcfeeb3f91a0aa056d3226
Aaron Longwell <aaron@eeepc.(none)>
2008-10-10 21:27:35 -0700

    Set up rSpec directory.

---
+++ lib/usgs.rb
@@ -0,0 +1,5 @@
+$:.unshift File.dirname(__FILE__)
+require 'usgs/water_data'
+
+module USGS
+end
--- lib/usgs/water_data.rb
+++ lib/usgs/water_data.rb
@@ -1,3 +1,18 @@
-class WaterData
-  VERSION = '0.0.1''
+module USGS
+  class WaterData
+    VERSION = '0.0.1'
+
+    class << self
+
+      def update_state(st)
+        require 'open-uri'
+        url = "http://waterdata.usgs.gov/#{st}/nwis/current/?type=flow&format=rdb"
+        open(url).each do |line|
+          puts line
+        end
+      end
+
+    end
+    
+  end
 end
